1d900912f06019766eb17ab76a0c37694d133674
Parth <parthlakhani112001@gmail.com>
2026-05-23 23:30:14 +0530

    Refactor package names to follow Java conventions

--- auth-service/src/main/java/com/tmp/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/tmp/authservice/AuthServiceApplication.java
@@ -1,13 +1,13 @@
-package com.tmp.task_service;
+package com.tmp.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskServiceApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaskServiceApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
--- task-service/src/main/java/com/tmp/taskservice/TaskServiceApplication.java
+++ task-service/src/main/java/com/tmp/taskservice/TaskServiceApplication.java
@@ -1,13 +1,13 @@
-package com.tmp.auth_service;
+package com.tmp.taskservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthServiceApplication {
+public class TaskServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
+		SpringApplication.run(TaskServiceApplication.class, args);
 	}
 
 }
