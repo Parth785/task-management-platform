d3041d11e52b56371a185e9358166e09c590dfe9
Parth <parthlakhani112001@gmail.com>
2026-05-23 23:25:32 +0530

    Initial microservices project structure setup

---
+++ auth-service/src/test/java/com/tmp/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmp.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
